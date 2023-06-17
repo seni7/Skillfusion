5b032beaa87776ecf28f0ff4369ad2bd6e43ca1b
Your Name <you@example.com>
2023-06-17 08:59:45 +0300

    chart

--- pages/DashboardPage.tsx
+++ pages/DashboardPage.tsx
@@ -510,16 +510,14 @@ function DashboardPage() {
               </ul>
             </div>
           </div>
-          <div className="flex flex-col row-span-3 bg-white shadow rounded-lg">
-            <div className="px-6 py-5 font-semibold border-b border-gray-100">Students by type of studying</div>
-            <div className="p-4 flex-grow">
-              <div className="flex items-center justify-center h-full px-4 py-24 text-gray-400 text-3xl font-semibold bg-gray-100 border-2 border-gray-200 border-dashed rounded-md">
-              <div className="flex justify-center w-full">
-        <canvas ref={myChartRef} ></canvas>
-      </div>
-                </div>
-            </div>
+          <div className="flex flex-col row-span-3 bg-blue-300 shadow rounded-lg">
+        <div className="px-6 py-5 font-semibold border-b border-gray-100">Students by type of studying</div>
+        <div className="p-4 flex-grow">
+          <div className="flex items-center justify-center h-full">
+            <canvas ref={myChartRef} style={{ width: '400px', height: '400px' }}></canvas>
           </div>
+        </div>
+      </div>
         </section>
        
       </main>
